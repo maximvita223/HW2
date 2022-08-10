e89f889e2c01e85e80b36956f3134e39132f59b9
Maxim <maks.bidenko.96@mail.ru>
2022-08-10 22:12:19 +0300

    сделал 3 задание

---
+++ dz3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 5 && day < 8)
+{
+    Console.WriteLine("Выходной");
+}
+if (day <= 5)
+{
+    Console.WriteLine("Не выходной");
+}
