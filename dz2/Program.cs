8fedbd10329cfaab45f5bbf5e6c48f49eb846d42
Maxim <maks.bidenko.96@mail.ru>
2022-08-10 22:02:15 +0300

    сделал 2 задание

---
+++ dz2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 99 && num >= 0)
+Console.WriteLine("нет третьего числа");
+    else
+    {
+        while (num > 1000) 
+        {
+            num = num / 10;
+        }
+        num = num % 10;
+        Console.WriteLine(num);
+    }
