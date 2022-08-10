4516049eeabf93b6089715b3c48b85c7fcbce07a
Maxim <maks.bidenko.96@mail.ru>
2022-08-10 22:15:48 +0300

    Fixed

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num < 100 || num > 1000)
+if(num < 100 || num > 999)
 Console.WriteLine("ERROR");
 else
 num = (num / 10);
